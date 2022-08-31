ee3df4b840f4f8a0e0e1913ffec231cd5f973208
Macroflai <49238165+Topikazge@users.noreply.github.com>
2022-08-31 12:09:30 +0300

    zavorotnik

--- Assets/Scripts/Profile/AvatarManager.cs
+++ Assets/Scripts/Profile/AvatarManager.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using System.Linq;
-public class AvatarManager : MonoBehaviour
+using Unity.VisualScripting;
+
+public class AvatarManager : WindowActive
 {
+    public FileInfo[] FileS
+    {
+        get
+        {
+            Debug.Log("Return  files");
+            return files;
+        }
+        set
+        {
+            Debug.Log("set  files value: " + value);
+            files = value;
+        }
+    }
+    public static string PathToImage;
     [SerializeField] private ProfileCreator _creator;
     public GameObject fileListPan, fileContent, filePrefab;
     public RawImage avatarImg;
     private DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\Macroflai\Documents\GitHub\FirstSite\Image\Portfolio");
-  // private DirectoryInfo dirInfo = new DirectoryInfo("/mnt/sdcard");
+    // private DirectoryInfo dirInfo = new DirectoryInfo("/mnt/sdcard");
     private FileInfo[] files;
     private GameObject[] instanceObjs;
 
@@ -27,35 +43,27 @@ public class AvatarManager : MonoBehaviour
 
     public void LoadAvatarsList()
     {
-        Debug.Log("LoadAvatarsList: ");
         fileListPan.SetActive(true); avatarImg.gameObject.SetActive(false);
-        files = new string[] { "*.jpeg", "*.jpg", "*.png" }.SelectMany(ext => dirInfo.GetFiles(ext, SearchOption.AllDirectories)).ToArray();
-        instanceObjs = new GameObject[files.Length];
-        for (int i = 0; i < files.Length; i++)
+        FileS = new string[] { "*.jpeg", "*.jpg", "*.png" }.SelectMany(ext => dirInfo.GetFiles(ext, SearchOption.AllDirectories)).ToArray();
+        instanceObjs = new GameObject[FileS.Length];
+        for (int i = 0; i < FileS.Length; i++)
         {
             FileScript file = Instantiate(filePrefab, fileContent.transform).GetComponent<FileScript>();
-            file.fileNameText.text = files[i].Name;
-            file.index = 1;
+            file.fileNameText.text = FileS[i].Name;
+            file.index = i;
             instanceObjs[i] = file.gameObject;
         }
-
-
+        Debug.Log("file in Load: " + FileS.Length);
     }
     public void SelectAvatar(int index)
     {
-<<<<<<< HEAD
-        Debug.Log("files: " + files);
-        WWW www = new WWW("file://" + files[index].FullName);
+        Debug.Log("file in Load: " + FileS.Length);
+        WWW www = new WWW("file://" + FileS[index].FullName);
         avatarImg.texture = www.texture;
         PathToImage = www.url;
-        Debug.Log("Ïóòü ê ôàéëó: " + PathToImage);
-=======
-        Debug.Log("Âûâàûâàûâàûâàûâàûâààâàó");
-        WWW www = new WWW("file://" + files[index].FullName);
-        avatarImg.texture = www.texture;
->>>>>>> parent of 017fbff (Ð¾Ð»Ñ€Ð»Ñ€Ð»Ñ€)
         fileListPan.SetActive(false); avatarImg.gameObject.SetActive(true);
         foreach (GameObject obj in instanceObjs)
             Destroy(obj);
+        _creator.CloseFile();
     }
 }
--- Assets/Scripts/Profile/ProfileCreator.cs
+++ Assets/Scripts/Profile/ProfileCreator.cs
@@ -9,8 +9,10 @@ using System.IO;
 public class ProfileCreator : MonoBehaviour, IWindowActive
 {
     [SerializeField] private TextMeshProUGUI _nameField;
-    [SerializeField] private Image _imageField;
+    [SerializeField] private RawImage _imageField;
     [SerializeField] private Windowcontroler _controllerEnterWidow;
+    [SerializeField] private ProfileCreator _profileWindow;
+    [SerializeField] private AvatarManager _FileWindow;
     public void Hide()
     {
         gameObject.SetActive(false);
@@ -27,51 +29,29 @@ public class ProfileCreator : MonoBehaviour, IWindowActive
     }
 
    
+    public void CloseFile()
+    {
+        _FileWindow.Hide();
+        _profileWindow.View();
+    }
+
+    public void OpenFiles()
+    {
+        _profileWindow.Hide();
+        _FileWindow.View();
+    }
 
     private void SaveChange()
     {
-		/*string nameUser = _nameField.text;
-        Sprite imageUser = _imageField.sprite;
-       // Texture2D itemBGTex = imageUser.texture;
-       // byte[] itemBGBytes = itemBGTex.EncodeToPNG();
-      //  string path = "/Resources" + nameUser + ".png";
-      //  File.WriteAllBytes(path, itemBGBytes);
+
+		string nameUser = _nameField.text;
+        Texture2D imageUser = (Texture2D)_imageField.texture;
+        Debug.Log(AvatarManager.PathToImage);
+        // byte[] itemBGBytes = imageUser.EncodeToPNG();
+        //string path = "\\Resources\\" + nameUser + ".png";
+        // File.WriteAllBytes(path, itemBGBytes);
         ChangeUserProfil changeUserProfil = new ChangeUserProfil();
-        changeUserProfil.SetUser(UserContainer.Instance).ChangeName(nameUser).ChangeIcon("Assets/Resources/1.png").Save();
-        _controllerEnterWidow.OpenMain(this);*/
+        changeUserProfil.SetUser(UserContainer.Instance).ChangeName(nameUser).ChangeIcon(AvatarManager.PathToImage).Save();
+        _controllerEnterWidow.OpenMain(this);
     }
-
-	void OpenFileBrowser()
-	{
-		
-		StartCoroutine(ShowLoadDialogCoroutine());
-	}
-
-	IEnumerator ShowLoadDialogCoroutine()
-	{
-		// Show a load file dialog and wait for a response from user
-		// Load file/folder: both, Allow multiple selection: true
-		// Initial path: default (Documents), Initial filename: empty
-		// Title: "Load File", Submit button text: "Load"
-		yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.FilesAndFolders, true, null, null, "Load Files and Folders", "Load");
-
-		// Dialog is closed
-		// Print whether the user has selected some files/folders or cancelled the operation (FileBrowser.Success)
-		Debug.Log(FileBrowser.Success);
-
-		if (FileBrowser.Success)
-		{
-			// Print paths of the selected files (FileBrowser.Result) (null, if FileBrowser.Success is false)
-			for (int i = 0; i < FileBrowser.Result.Length; i++)
-				Debug.Log(FileBrowser.Result[i]);
-
-			// Read the bytes of the first file via FileBrowserHelpers
-			// Contrary to File.ReadAllBytes, this function works on Android 10+, as well
-			byte[] bytes = FileBrowserHelpers.ReadBytesFromFile(FileBrowser.Result[0]);
-
-			// Or, copy the first file to persistentDataPath
-			string destinationPath = Path.Combine(Application.persistentDataPath, FileBrowserHelpers.GetFilename(FileBrowser.Result[0]));
-			FileBrowserHelpers.CopyFile(FileBrowser.Result[0], destinationPath);
-		}
-	}
 }
