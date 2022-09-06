4ed4d56e46e03a02d162aaaf8e06ffc6a12734e1
Macroflai <49238165+Topikazge@users.noreply.github.com>
2022-09-06 08:42:04 +0300

    Сделал выбор аватарки

--- Assets/Scripts/Profile/FileScript.cs
+++ Assets/Scripts/Profile/FileScript.cs
@@ -5,8 +5,16 @@ using TMPro;
 public class FileScript : MonoBehaviour
 {
     public TextMeshProUGUI fileNameText;
+    [SerializeField] private Sprite _image;
+    [SerializeField] private Image _imageUI;
 
     public int index;
+
+    public void Awake()
+    {
+        _imageUI.sprite = _image;
+    }
+
     public void OnClick()
     {
         Debug.Log("Вызывю картинку");
